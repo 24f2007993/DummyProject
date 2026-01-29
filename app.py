dc8c01853eca2519a626a5295e79f361af5f08da
20f20-pixel <24f2007993@ds.study.iitm.ac.in>
2026-01-29 16:31:13 +0530

    day2

--- app.py
+++ app.py
@@ -9,5 +9,14 @@ def login():
 @app.route('/signUp')
 def signUp():
     return render_template('signUp.html')
+@app.route('/createProduct')
+def create():
+    return render_template('create_product.html')
+@app.route('/updateProduct')
+def update():
+    return render_template('update_product.html')
+@app.route('/requestProduct')
+def request():
+    return render_template('request.html')
 if __name__ == '__main__':
     app.run(debug = True)
